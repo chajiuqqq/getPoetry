a65fbb639ddb47501b55cbd738f0052de971e571
chajiuqqq <chajiuqqq@outlook.com>
2020-03-15 12:06:31 +0800

    complete to submit

--- src/main/java/com/chajiu/util/LangConverter.java
+++ src/main/java/com/chajiu/util/LangConverter.java
@@ -1,10 +1,11 @@
 package com.chajiu.util;
 
+
 import com.spreada.utils.chinese.ZHConverter;
 
 public class LangConverter {
 
-    private static ZHConverter toSimpleConverter=ZHConverter.getInstance(ZHConverter.SIMPLIFIED);
+    private static ZHConverter toSimpleConverter= ZHConverter.getInstance(ZHConverter.SIMPLIFIED);
     private static ZHConverter toTraditionConverter=ZHConverter.getInstance(ZHConverter.TRADITIONAL);
 
     public static String toSimple(String in){
