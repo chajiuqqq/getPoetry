eb7b37127f17e5813fc44d126a2fff058b0029ab
chajiuqqq <chajiuqqq@outlook.com>
2020-04-08 10:13:39 +0800

    first merge together, display correctly but need improvement

--- src/main/resource/static/js/categories.js
+++ src/main/resource/static/js/categories.js
@@ -1,15 +1,17 @@
  Vue.component('categories-component',{
 			template:`
 			<div class="rightkuang">
-				<div style="font-size:23px;font-family:NSimSun;font-weight:bold;color:#19537D;margin-top:10px;margin-left:30px">诗词分类</div>
-		          	<div style="height:10px"></div>
-		          	<div style="margin-left:15px;">
+				<div style="font-size:2.3rem;font-family:NSimSun;font-weight:bold;color:#19537D;margin-top:1rem;margin-left:3rem">诗词分类</div>
+		          <div style="height:1rem"></div>
+		          	<div style="margin-left:1.5rem;">
 						<el-button size="small" round v-for="item in message" style="color:#19537D;backgroundColor:transparent;
-						margin-left:25px;margin-top:10px" :buttonid="item.id" @click="toSortPage($event)">{{item.name}}
+						margin-left:2.5rem;margin-top:1rem" :buttonid="item.id" @click="toSortPage($event)">{{item.name}}
 						</el-button>
 					</div>
 					
-					<div style="height:20px"></div>
+					<div style="height:2rem"></div>
+
+
 			</div>
 			`,
 			data(){
@@ -19,7 +21,7 @@
 			},
 			methods:{
 				toSortPage(e){
-					window.location.href='/view/sort.html?bid='+e.currentTarget.getAttribute('buttonid');
+					window.location.href='sort.html?bid='+e.currentTarget.getAttribute('buttonid');
 				},
 				jumppoem(){
 	
@@ -45,4 +47,4 @@
 			mounted:function(){
 				this.jumppoem();
 			}
-		})
+		});
--- src/main/resource/static/js/search.js
+++ src/main/resource/static/js/search.js
@@ -4,89 +4,130 @@
 
 Vue.component('common-search',{
 	props: ['searchinput'],
-	template:`<div style="display:inline-block;margin-top:8px;float:left;margin-left:5px;position: relative;">
+	template:`<div style="display:inline-block;margin-top:0.8rem;float:left;margin-left:0.5rem;position: relative;">
 					  <el-input @keyup.native="Keyup" placeholder="输入诗词分类或描述"  class="input-with-select"
 					  prefix-icon="el-icon-search" 
 					   v-model="input" 
-					  style="width:500px;margin-left:10px;border:2px solid #DF0101;
+					  style="width:40rem;margin-left:1rem;border:0.1rem solid #DF0101;
 					  font-family: NSimSun;">
 					     <el-button slot="append" @click="clicksearch" style="font-family:NSimSun;background-color:#D10101;color:white; "  >
 					     搜索诗词</el-button>
 					    </el-input>
 					    
 					    <div id="d1"  v-if="show"
-			        	style="border:solid lightgrey 2px;width: 400px;position: absolute;margin-left:10px;background-color:white">
-							<table style="width: 100%" >
-								<tr v-for="item in message" @mouseover="MouseOver($event)" 
-			        	@mouseout="MouseLeave($event)"><td :cid='item.id' @click="getpoems($event)">{{item.name}}</td></tr>
-							</table>
+			        	style="border:solid lightgrey 0.2rem;box-shadow: 0 0.2rem 1.2rem 0 rgba(0, 0, 0, 0.1);border-radius: 0.5rem;
+			        	position: absolute;z-index:9999;width:30rem;margin-left:1rem;background-color:white"
+			        	>
+							<div style="height:1rem"></div>
+			        		<span style="width:15%;text-align:center;margin-left:5%">{{message1}}</span>
+							<table style="width:80%;margin-left:15%">
+								<tr v-for="item in sort"> 
+									<td style="padding-left:3%;border-left:solid 0.15rem grey;" 
+										@mouseover="MouseOver($event)" 
+			        					@mouseout="MouseLeave($event)" :sid='item.id' @click="getsearch($event)">{{item.name}}
+			        				</td>
+			        			</tr>
+			        		</table>
+			        		<hr/>
+			        		<span style="width:15%;text-align:center;margin-left:5%;">{{message2}}</span>
+			        		<table style="width:80%;margin-left:15%">
+			        			<tr v-for="item in poem" > 
+									<td style="padding-left:3%;border-left:solid 0.15rem grey;"
+										@mouseover="MouseOver($event)" 
+			        					@mouseout="MouseLeave($event)" :pid='item.id' @click="getpoem($event)">{{item.title}}&nbsp;-&nbsp;{{item.author}}
+			        				</td>
+			        			</tr>
+			        		</table>
+							<hr/>
+			        		<span style="width:10%;text-align:center;margin-left:5%">{{message3}}</span>
+			        		<table style="width:80%;margin-left:15%">
+			        			<tr v-for="item in author" > 
+									<td style="padding-left:3%;border-left:solid 0.15rem grey;"
+										@mouseover="MouseOver($event)" 
+			        					@mouseout="MouseLeave($event)" :aid='item.id' @click="getauthor($event)">{{item.name}}
+			        				</td>
+			        			</tr>
+			        		</table>
+							<div style="height:2rem"></div>
 					    </div>
 					</div>`,
 	data(){
 		return{
 			input:'',
-			message:null,
 			show:false,
-			nameid:null
+			nameid:null,
+			sort:null,
+			poem:null,
+			author:null,
+			message1:"分类",
+			message2:"诗词",
+			message3:"作者"
 		}
 	},
 	methods:{
-		clicksearch(){
-					if(this.input==''){
-						this.$alert('请输入诗词分类或描述', '警告', {
-				          confirmButtonText: '确定',
-				          callback: action => {
-				            this.$message({
-				              type: 'info',
-				              message: `action: ${ action }`
-				            });
-				          }
-				        });
-					}else{
-					var encodename=encodeURI(this.input);
-					window.location.href='/view/search_searchpoem.html?title='+encodename;
-		}
+				clicksearch(){
+							if(this.input==''){
+								this.$alert('请输入诗词分类或描述', '警告', {
+						          confirmButtonText: '确定',
+						          callback: action => {
+						            this.$message({
+						              type: 'info',
+						              message: `action: ${ action }`
+						            });
+						          }
+						        });
+							}else{
+							var encodename=encodeURI(this.input);
+							window.location.href='search_searchpoem.html?title='+encodename;
+				}
 				},
-	    MouseOver(e){
-	    	    console.log("moueseover");
-				e.target.style.backgroundColor='lightblue'
+			    MouseOver(e){
+			    	    console.log("moueseover");
+						e.target.style.backgroundColor='lightblue'
 
-	    },
-	    MouseLeave(e){
-	    	 e.target.style.backgroundColor = '';
-	    },
-	    Keyup(){
-					 var uri='/api/category/name/'+encodeURI(this.input);
-					 console.log(uri);
+			    },
+			    MouseLeave(e){
+			    	 e.target.style.backgroundColor = '';
+			    },
+	   			 Keyup(){
+					 var uri='/api/search/'+encodeURI(this.input);
 					 if(this.input!='')
 					 {
 					 	axios({
 					 	 method: 'get',
 						  url: uri,
 						 })
-				    .then(response =>
-				      { 
-				      	if(response.data.code==200)
-				      	{
-					        console.log(response)
-					      	this.message=response.data.data
-							
-							this.show=true
-				      	}else{
-				      		this.show=false
-				      	}
-				      	
-				      })
-				      .catch(function (error) { // 请求失败处理
-				        console.log(error);
-				      });
+					    .then(response =>
+					      { 
+					      	if(response.data.code==200)
+					      	{
+						        this.sort=response.data.data.categories;
+						      	this.poem=response.data.data.poetries;
+								this.author=response.data.data.authors;
+								
+								this.show=true
+					      	}else{
+					      		this.show=false
+					      	}
+					      	
+					      })
+					      .catch(function (error) { // 请求失败处理
+					        console.log(error);
+					      });
 					 }else{
 					 	this.show=false
 					 }
 				},
-				getpoems(e){
-					console.log('youyouyou');
-					window.location.href='/view/search_poem.html?id='+e.target.getAttribute('cid');
+				getsearch(e){
+					window.location.href='search_poem.html?id='+e.target.getAttribute('sid');
+					
+				},
+				getpoem(e){
+					window.location.href='poem.html?titleid='+e.target.getAttribute('pid');
+					
+				},
+				getauthor(e){
+					window.location.href='author.html?aid='+e.target.getAttribute('aid');
 					
 				}
 			},
